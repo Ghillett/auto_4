c9739d3958c0eeb6020360847496e9855a18d6a0
JP <email>
2023-01-23 22:33:47 +1000

    fix test

--- src/test/java/FirstCardDeliveryTest.java
+++ src/test/java/FirstCardDeliveryTest.java
@@ -1,4 +1,4 @@
-import org.junit.jupiter.api.BeforeEach;
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
@@ -10,29 +10,29 @@ import java.time.format.DateTimeFormatter;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$x;
 import static com.codeborne.selenide.Selenide.open;
-import static java.time.LocalDateTime.parse;
 
 public class FirstCardDeliveryTest {
 
-    String setNewDate(){
+    String setNewDate() {
         LocalDateTime now = LocalDateTime.now();
         now = now.plus(5, ChronoUnit.DAYS);
-        String newDate = DateTimeFormatter.ofPattern("dd/MM/yyyy kk:mm:ss").format(now);
+        String newDate = DateTimeFormatter.ofPattern("dd.MM.yyyy").format(now);
         return newDate;
     }
 
     @Test
-    void shouldTest(){
+    void shouldTest() {
         open("http://localhost:9999/");
+        String setDate = setNewDate();
         $x("//span[@data-test-id='city']//input").setValue("Казань");
         $x("//*[@data-test-id='date']//input").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $x("//*[@data-test-id='date']//input").setValue((setNewDate()));
+        $x("//*[@data-test-id='date']//input").setValue(setDate);
         $x("//*[@data-test-id='name']//input").setValue("Петров Иван");
         $x("//*[@data-test-id='phone']//input").setValue("+74935167891");
 
         $x("//*[@data-test-id='agreement']//span[@class='checkbox__box']").click();
         $x("//*[text() = 'Забронировать']").click();
 
-        $x("//*[contains(text(),'Встреча успешно забронирована')]").shouldBe(visible, Duration.ofSeconds(15));
+        $x("//*[@data-test-id='notification']").shouldHave(Condition.text("Встреча успешно забронирована на " + setDate), Duration.ofSeconds(15)).shouldBe(visible);
     }
 }
